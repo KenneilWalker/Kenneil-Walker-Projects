35809cc235138a21bf63b61d364e895b5e164fb7
kenne <kenne@LAPTOP-50K62JPI>
2024-10-29 16:14:35 -0400

    Test cases for new offers.

--- orders-service/src/test/java/com/kenneil/orders_service/controller/OrdersControllerTest.java
+++ orders-service/src/test/java/com/kenneil/orders_service/controller/OrdersControllerTest.java
@@ -18,14 +18,65 @@ public class OrdersControllerTest {
 
 	@Test
 	@DisplayName("junit test case for posttItemsFromOrder in OrdersController class ")
-	public void getItemsFromOrder() throws Exception {
+	public void Test_ItemsFromOrder() throws Exception {
 
 		String requestBody = "{\"apples\": 2, \"oranges\": 2}";
 
-		mockMvc.perform(MockMvcRequestBuilders.post("/orders/order").contentType(MediaType.APPLICATION_JSON)
-				.content(requestBody)).andExpect(jsonPath("$.apples", is(2))).andExpect(jsonPath("$.oranges", is(2)))
+		mockMvc.perform(MockMvcRequestBuilders.post("/orders/order")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(requestBody))
+				.andExpect(jsonPath("$.apples", is(2))).andExpect(jsonPath("$.oranges", is(2)))
 				.andExpect(jsonPath("$.totalCost", is(1.7)));
 
 	}
+	
+	@Test
+	public void Test_BuyOneGetOneFreeApples() throws Exception{
+		String requestBody = "{\"apples\": 4, \"oranges\": 0}";
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/orders/order/sale")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(requestBody))
+				.andExpect(jsonPath("$.apples", is(8)))
+				.andExpect(jsonPath("$.oranges", is(0)))
+				.andExpect(jsonPath("$.totalCost", is(2.4)));
+	}
+	
+	@Test
+	public void Test_BuyThreeOrangesForThePriceOfTwo() throws Exception{
+		String requestBody = "{\"apples\": 0, \"oranges\": 3}";
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/orders/order/sale")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(requestBody))
+				.andExpect(jsonPath("$.apples", is(0)))
+				.andExpect(jsonPath("$.oranges", is(3)))
+				.andExpect(jsonPath("$.totalCost", is(0.5)));
+	}
+	
+	@Test
+	public void Test_BothAppleAndOrangeOffers() throws Exception{
+		String requestBody = "{\"apples\": 6, \"oranges\": 9}";
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/orders/order/sale")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(requestBody))
+				.andExpect(jsonPath("$.apples", is(12)))
+				.andExpect(jsonPath("$.oranges", is(9)))
+				.andExpect(jsonPath("$.totalCost", is(5.1)));
+	}
+	
+	@Test
+	public void Test_EmptyOrdersForBoth() throws Exception{
+		String requestBody = "{\"apples\": 0, \"oranges\": 0}";
+
+		mockMvc.perform(MockMvcRequestBuilders.post("/orders/order/sale")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(requestBody))
+				.andExpect(jsonPath("$.apples", is(0)))
+				.andExpect(jsonPath("$.oranges", is(0)))
+				.andExpect(jsonPath("$.totalCost", is(0.0)));
+	}
+	
 
 }
